caa24efe2c62e16b3526e66650c9610bbc885751
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-10-22 22:50:02 +0800

    Change base path in prepare_edit.sh
    Updated the base path in prepare_edit.sh to point to the current test case directory.

--- example_case/example_1/scripts/prepare_edit.sh
+++ example_case/example_1/scripts/prepare_edit.sh
@@ -1,1 +1,1 @@
-python prepare_edit_tools/create_train_splits.py --base_path /data1/chenyuxin/code/Infinity_clone/example_case/example_1/
+python prepare_edit_tools/create_train_splits.py --base_path ./EditInfinity/example_case/example_1/  #Set the current test case path
