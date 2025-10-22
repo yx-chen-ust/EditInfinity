6f9274925b42ea6cae9c81d011241f819a774018
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-10-22 23:06:35 +0800

    Fix PYTHONPATH export and echo statement

--- example_case/example_2/scripts/edit_pipeline.sh
+++ example_case/example_2/scripts/edit_pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=$PYTHONPATH:  #设置相对路径
+export PYTHONPATH=$PYTHONPATH:""  #Set its path ./EditInfinity as a relative path
 
 set -euo pipefail
 set -x
@@ -80,4 +80,3 @@ update_var "$INFER_SH" use_lora_iter 50
 bash "$INFER_SH"
 
 echo "Pipeline finished successfully."
-
