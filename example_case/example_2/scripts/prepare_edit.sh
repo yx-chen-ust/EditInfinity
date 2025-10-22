ac8bd6dc971838473da61965502500fe0034ecc9
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-10-22 23:07:39 +0800

    Update prepare_edit.sh

--- example_case/example_2/scripts/prepare_edit.sh
+++ example_case/example_2/scripts/prepare_edit.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=2   #设置GPU
-export PYTHONPATH=$PYTHONPATH:/data1/chenyuxin/code/Infinity_clone/  #设置相对路径
+export CUDA_VISIBLE_DEVICES=0   #set GPU
+export PYTHONPATH=$PYTHONPATH:""  #Set its path ./EditInfinity as a relative path
 
-python prepare_edit_tools/create_train_splits.py --base_path /data1/chenyuxin/code/Infinity_clone/example_case/example_2/
+python prepare_edit_tools/create_train_splits.py --base_path ./EditInfinity/example_case/example_2/
 
-python prepare_edit_tools/generate_weighted_tensor.py --case_path /data1/chenyuxin/code/Infinity_clone/example_case/example_2/
+python prepare_edit_tools/generate_weighted_tensor.py --case_path ./EditInfinity/example_case/example_2/
 
 python prepare_edit_tools/get_each_scale_code.py \
   --base_path example_case/example_2/ \
