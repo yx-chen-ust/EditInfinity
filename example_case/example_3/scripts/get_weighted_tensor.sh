d54ccbc0f5968edbb5535c3743b6df626134c78d
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-10-22 23:21:22 +0800

    Update GPU settings and paths in script

--- example_case/example_3/scripts/get_weighted_tensor.sh
+++ example_case/example_3/scripts/get_weighted_tensor.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=2   #设置GPU
-export PYTHONPATH=$PYTHONPATH:/data1/chenyuxin/code/Infinity_clone/  #设置相对路径
+export CUDA_VISIBLE_DEVICES=0   #set GPU
+export PYTHONPATH=$PYTHONPATH:""  #Set its path ./EditInfinity as a relative path
 
 python prepare_edit_tools/visualize_attention_map.py \
-    --base_path /data1/chenyuxin/code/Infinity_clone/example_case/example_3 \
+    --base_path ./EditInfinity/example_case/example_3 \
     --threshold 0.5
 
 python prepare_edit_tools/generate_weighted_tensor.py \
-    --case_path /data1/chenyuxin/code/Infinity_clone/example_case/example_3
+    --case_path ./EditInfinity/example_case/example_3
