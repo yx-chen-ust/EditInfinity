75ef15cfae0c384d041f12e9b47e5c50ee007678
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-11-25 00:25:53 +0800

    Update paths and VAE weight file in prepare_edit.sh

--- example_case/example_2/scripts/prepare_edit.sh
+++ example_case/example_2/scripts/prepare_edit.sh
@@ -1,10 +1,9 @@
 export CUDA_VISIBLE_DEVICES=0   #set GPU
-export PYTHONPATH=$PYTHONPATH:""  #Set its path ./EditInfinity as a relative path
 
-python prepare_edit_tools/create_train_splits.py --base_path ./EditInfinity/example_case/example_2/
+python prepare_edit_tools/create_train_splits.py --base_path ./example_case/example_2/
 
-python prepare_edit_tools/generate_weighted_tensor.py --case_path ./EditInfinity/example_case/example_2/
+python prepare_edit_tools/generate_weighted_tensor.py --case_path ./example_case/example_2/
 
 python prepare_edit_tools/get_each_scale_code.py \
   --base_path example_case/example_2/ \
-  --vae_path weights/infinity_vae_d32_reg.pth 
+  --vae_path weights/infinity_vae_d32reg.pth 
