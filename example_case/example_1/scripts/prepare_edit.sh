61e60399d950bde4a2815f3eddb0d451f2dd7069
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-11-25 00:09:55 +0800

    Fix base path in prepare_edit.sh script

--- example_case/example_1/scripts/prepare_edit.sh
+++ example_case/example_1/scripts/prepare_edit.sh
@@ -1,1 +1,1 @@
-python prepare_edit_tools/create_train_splits.py --base_path ./EditInfinity/example_case/example_1/  #Set the current test case path
+python prepare_edit_tools/create_train_splits.py --base_path ./example_case/example_1/  #Set the current test case path
