24faf2b1e1a2216269592873c3c80c1942fc7e85
yuxin chen <107035779+yx-chen-ust@users.noreply.github.com>
2025-11-25 16:25:01 +0800

    Update get_weighted_tensor.sh

--- example_case/example_3/scripts/get_weighted_tensor.sh
+++ example_case/example_3/scripts/get_weighted_tensor.sh
@@ -1,9 +1,8 @@
 export CUDA_VISIBLE_DEVICES=0   #set GPU
-export PYTHONPATH=$PYTHONPATH:""  #Set its path ./EditInfinity as a relative path
 
 python prepare_edit_tools/visualize_attention_map.py \
-    --base_path ./EditInfinity/example_case/example_3 \
+    --base_path ./example_case/example_3 \
     --threshold 0.5
 
 python prepare_edit_tools/generate_weighted_tensor.py \
-    --case_path ./EditInfinity/example_case/example_3
+    --case_path ./example_case/example_3
